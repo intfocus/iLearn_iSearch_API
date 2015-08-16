e84b2f2e86d1807386eb1ea87a3dc3b453444f0c
ericyueyi <yy_lfy@126.com>
2015-08-16 22:02:36 +0800

    Eric 新增

---
+++ Training/TrainingManagers_roll_list.php
@@ -0,0 +1,392 @@
+<?php
+   require_once("TrainingManagers_utility.php");
+
+   define("FILE_NAME", "../DB.conf");
+   define("DELAY_SEC", 3);
+   define("FILE_ERROR", -2);
+   
+   if (file_exists(FILE_NAME))
+   {
+      include(FILE_NAME);
+   }
+   else
+   {
+      sleep(DELAY_SEC);
+      echo FILE_ERROR;
+      return;
+   }
+   
+   try{
+      // TODO: 从 Session 里面拿到 login_name + user_id
+      session_start();
+      if (isset($_SESSION["GUID"]) == "" || isset($_SESSION["username"]) == "")
+      {
+         session_write_close();
+         sleep(DELAY_SEC);
+         header("Location:". $web_path . "main.php?cmd=err");
+         exit();
+      }
+   }
+   catch(exception $ex)
+   {
+      session_write_close();
+      sleep(DELAY_SEC);
+      header("Location:". $web_path . "main.php?cmd=err");
+      exit();
+   }
+   $user_id = $_SESSION["GUID"];
+   $login_name = $_SESSION["username"];
+   // $login_name = "Phantom";
+   // $user_id = 1;
+   $current_func_name = "iSearch";
+   session_write_close();
+   
+   //query          
+   $link;
+   $db_host;
+   $admin_account;
+   $admin_password;
+   $connect_db;
+   $str_query;
+   $str_query1;
+   $result;                 //query result
+   $result1;
+   $row;                    //result data array
+   $row1;
+   $row_number;
+   $refresh_str;
+   
+   header('Content-Type:text/html;charset=utf-8');
+   
+   //define
+   define("DB_HOST", $db_host);
+   define("ADMIN_ACCOUNT", $admin_account);
+   define("ADMIN_PASSWORD", $admin_password);
+   define("CONNECT_DB", $connect_db);
+   define("TIME_ZONE", "Asia/Shanghai");
+   define("ILLEGAL_CHAR", "'-;<>");                         //illegal char
+
+   //return value
+   define("DB_ERROR", -1);
+   define("SYMBOL_ERROR", -3);
+   define("SYMBOL_ERROR_CMD", -4);
+   define("MAPPING_ERROR", -5);
+   
+   //timezone
+   date_default_timezone_set(TIME_ZONE);
+
+   //----- Check number -----
+   function check_number($check_str)
+   {
+      if(!is_numeric($check_str))
+      {
+         return SYMBOL_ERROR; 
+      }
+      if($check_str < 0)
+      {
+         return SYMBOL_ERROR;
+      }
+      return $check_str;
+   }
+
+   //get data from client
+   $cmd;
+   $UserId;
+
+   //query
+   $link;
+
+   //link    
+   $link = @mysqli_connect(DB_HOST, ADMIN_ACCOUNT, ADMIN_PASSWORD, CONNECT_DB);    
+   if (!$link)  //connect to server failure    
+   {
+      sleep(DELAY_SEC);
+      echo DB_ERROR;       
+      return;
+   }
+
+   if (($training_id = check_number($_GET["TrainingId"])) == SYMBOL_ERROR)
+   {
+      sleep(DELAY_SEC);
+      echo SYMBOL_ERROR;
+      return;
+   }
+   
+   $userarray = array();
+   
+   function get_employ_id_from_user_id($userids)
+   {
+      if(strlen($userids)>0){
+         $userids = substr($userids,1);
+         $userids = substr($userids,0,-1);
+         $userids = str_replace(",,",",",$userids);
+      }
+      $link = @mysqli_connect(DB_HOST, ADMIN_ACCOUNT, ADMIN_PASSWORD, CONNECT_DB);    
+      if (!$link)  //connect to server failure    
+      {
+         sleep(DELAY_SEC);
+         echo DB_ERROR;
+         die("连接DB失败");
+      }
+      
+      $str_query = "select * from users where UserId in ($userids)";
+      if($result = mysqli_query($link, $str_query)){
+         while($row = mysqli_fetch_assoc($result)){
+            echo $row["EmployeeId"] . "\n";
+         }
+         return;
+      }
+      else
+      {
+         echo DB_ERROR;
+         die("操作资料库失败");
+      }
+   }
+   
+   
+?>
+
+<!DOCTYPE html>
+<html lang="zh-CN">
+    <head>
+        <meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE9">
+<meta http-equiv="Pragma" content="no-cache">
+<meta http-equiv="Expires" content="Tue, 01 Jan 1980 1:00:00 GMT">
+<link type="image/x-icon" href="../images/wutian.ico" rel="shortcut icon">
+<link rel="stylesheet" type="text/css" href="../lib/yui-cssreset-min.css">
+<link rel="stylesheet" type="text/css" href="../lib/yui-cssfonts-min.css">
+<link rel="stylesheet" type="text/css" href="../css/OSC_layout_new.css">
+<link type="text/css" href="../lib/jQueryDatePicker/jquery-ui.custom.css" rel="stylesheet" />
+<!-- for tree view -->
+<!-- End of tree view -->
+<!--[if lt IE 10]>
+<script type="text/javascript" src="lib/PIE.js"></script>
+<![endif]-->
+
+        <!-- Bootstrap core CSS -->
+        <link href="../newui/css/bootstrap.min.css" rel="stylesheet">
+        <link href="../newui/css/bootstrap-reset.css" rel="stylesheet">
+
+        <!--Animation css-->
+        <link href="../newui/css/animate.css" rel="stylesheet">
+
+        <!--Icon-fonts css-->
+        <link href="../newui/assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
+        <link href="../newui/assets/ionicon/css/ionicons.min.css" rel="stylesheet" />
+
+        <!--Morris Chart CSS -->
+        <link rel="stylesheet" href="../newui/assets/morris/morris.css">
+
+        <!-- sweet alerts -->
+        <link href="../newui/assets/sweet-alert/sweet-alert.min.css" rel="stylesheet">
+
+        <!-- Custom styles for this template -->
+        <link href="../newui/css/style.css" rel="stylesheet">
+        <link href="../newui/css/helper.css" rel="stylesheet">
+        <link href="../newui/css/style-responsive.css" rel="stylesheet" />
+		
+<title>武田 - 上传课程负责人员名单页面</title>
+<!-- BEG_ORISBOT_NOINDEX -->
+<Script Language=JavaScript>
+function lockFunction(obj, n)
+{
+   if (g_defaultExtremeType[n] == 1)
+      obj.checked = true;
+} 
+
+function click_logout()  //log out
+{
+   document.getElementsByName("logoutform")[0].submit();
+}
+
+function loaded()
+{  
+}
+
+//***Step12 修改页面点击保存按钮出发Ajax动作
+function modifyUsersContent(training_id)
+{
+   newUsersBatchInput = document.getElementsByName("newUsersBatchInput")[0].value.trim();
+   users_id = newUsersBatchInput.match(/[^\r\n]+/g);
+   
+   if (users_id)
+   {
+      for (i=0; i<users_id.length; i++)
+      {
+         users_id[i] = trim_start_end(users_id[i]);
+         if (!users_id[i].match(/^[0-9|a-zA-Z]+$/))
+         {
+            alert(users_id[i] + "有非数字及英文之字元");
+            return;
+         }
+      }
+   }
+   else
+   {
+      if(users_id == null || users_id.length == 0)
+      {
+         alert("没有输入员工号!");
+         window.close();
+         return;
+      }
+      confirm("没有输入员工号时，会刪除此考卷的已有名单。确定刪除？");
+   }
+
+   url_str = "TrainingManagers_update_roll.php";
+   $.ajax
+   ({
+      beforeSend: function()
+      {
+         //alert(str);
+      },
+      type: "POST",
+      url: url_str,
+      data: {
+               "training_id": training_id,
+               "users_id[]": users_id,
+            },
+      cache: false,
+      dataType: 'json',
+      success: function(res)
+      {
+         res = String(res);
+         if (res.match(/^-\d+$/))
+         {
+            alert("新增 / 删除课程包用户名单失败");
+         }
+         else if (res != 0)  //failed
+         {
+            alert(res);
+         }
+         else  //success
+         {
+            alert("用户批次新增 / 删除成功，页面关闭后请自行刷新");
+            window.close();
+         }
+      },
+      error: function(xhr)
+      {
+         alert("ajax error: " + xhr.status + " " + xhr.statusText);
+      }
+   });
+}
+</Script>
+<!--Step15 新增修改页面    起始 -->
+</head>
+<body Onload="loaded();">
+        <!--Main Content Start -->
+        <div class="" id="content">
+            
+            <!-- Header -->
+            <header class="top-head container-fluid">
+                <button type="button" class="navbar-toggle pull-left">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                
+                
+                <!-- Left navbar -->
+                <nav class=" navbar-default hidden-xs" role="navigation">
+                    <ul class="nav navbar-nav">
+
+                        <li><a href="#"><?php echo date('Y-m-d',time()); ?></a></li>
+                    </ul>
+                </nav>
+                
+                <!-- Right navbar -->
+                <ul class="list-inline navbar-right top-menu top-right-menu">  
+
+                    <!-- user login dropdown start-->
+                    <li class="dropdown text-center">
+              	
+						   <input type="hidden" id="userid" value="<?php echo $user_id ?>" />
+						   <form name=logoutform action=logout.php>
+						   </form>
+                        <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+                            <i class="fa fa-user"></i>
+                            <span class="username"><?php echo $login_name ?> </span> <span class="caret"></span>
+                        </a>
+                        <ul class="dropdown-menu extended pro-menu fadeInUp animated" tabindex="5003" style="overflow: hidden; outline: none; display:none;">
+                            <li><a href="javascript:void(0)" OnClick="click_logout();"><i class="fa fa-sign-out"></i> 退出</a></li>
+                        </ul>
+                    </li>
+                    <!-- user login dropdown end -->       
+                </ul>
+                <!-- End right navbar -->
+
+            </header>
+            <!-- Header Ends -->
+
+
+            <!-- Page Content Start -->
+            <!-- ================== -->
+
+            <div class="wraper container-fluid">
+                <div class="page-title"> 
+                    <h3 class="title">批次上传课程负责人员名单</h3> 
+                </div>
+
+                <!-- Basic Form Wizard -->
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="panel panel-default">
+                            <div class="panel-body"> 
+
+   <div id="exam_id" style="disply:none;"></div>
+   <table class="searchField" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+         <th>批次上传：(一行一笔数据，数据格式为 "工号"。直接在表格上修改考试人员异动)</th>
+      </tr>
+      <tr>
+         <td><Textarea name=newUsersBatchInput rows=30 cols=100 placeholder="员工号 如 E99999"><?   
+            // read roll
+         $str_query = "select * from Trainings where TrainingId=$training_id AND Status=".ACTIVE;
+         if($result = mysqli_query($link, $str_query)){
+            $row_number = mysqli_num_rows($result);
+            $row = mysqli_fetch_assoc($result);
+            $userids = $row["TrainingManager"];
+            if(strlen($userids)>0){
+               get_employ_id_from_user_id($userids);
+            }
+         }?></Textarea></td>        
+      </tr>   
+      <tr>
+         <th colspan="4" class="submitBtns">
+            <a class="btn_submit_new modifyUsersContent"><input name="modifyUsersButton" type="button" value="保存" OnClick="modifyUsersContent(<?echo $training_id?>)"></a>
+         </th>
+      </tr>        
+   </table>
+
+							</div>  <!-- End panel-body -->
+                        </div> <!-- End panel -->
+                    </div> <!-- end col -->
+                </div> <!-- End row -->
+
+				
+            </div>
+            <!-- Page Content Ends -->
+            <!-- ================== -->
+
+            <!-- Footer Start -->
+            <footer class="footer">
+                2015 © Takeda.
+            </footer>
+            <!-- Footer Ends -->
+
+
+
+        </div>
+        <!-- Main Content Ends -->
+
+<script type="text/javascript" src="../lib/jquery.easyui.min.js"></script>
+<script type="text/javascript" src="../lib/jquery.min.js"></script>
+<script type="text/javascript" src="../lib/jquery-ui.min.js"></script>
+<script type="text/javascript" src="../js/OSC_layout.js"></script>
+<script type="text/javascript" src="../js/utility.js"></script>
+
+</body>
+</html>
+<!--Step15 新增修改页面    结束 -->
---
+++ Training/Trainings_update_roll.php
@@ -0,0 +1,260 @@
+<?php
+   define("FILE_NAME", "../DB.conf");
+   define("DELAY_SEC", 3);
+   define("FILE_ERROR", -2);
+   define("SUCCESS", 0);
+   
+   if (file_exists(FILE_NAME))
+   {
+      include(FILE_NAME);
+   }
+   else
+   {
+      sleep(DELAY_SEC);
+      echo FILE_ERROR;
+      return;
+   }
+   $login_name = "Phantom";
+   
+   //query          
+   $link;
+   $db_host;
+   $admin_account;
+   $admin_password;
+   $connect_db;
+   $str_query;
+   $str_query1;
+   $result;                 //query result
+   $result1;
+   $row;                    //result data array
+   $row1;
+   $row_number;
+   $refresh_str;
+
+   header('Content-Type:text/html;charset=utf-8');
+
+   //define
+   define("DB_HOST", $db_host);
+   define("ADMIN_ACCOUNT", $admin_account);
+   define("ADMIN_PASSWORD", $admin_password);
+   define("CONNECT_DB", $connect_db);
+   define("TIME_ZONE", "Asia/Shanghai");
+   define("ILLEGAL_CHAR", "'-;<>");                         //illegal char
+
+   //return value
+   define("DB_ERROR", -1);
+   define("SYMBOL_ERROR", -3);
+   define("SYMBOL_ERROR_CMD", -4);
+   define("MAPPING_ERROR", -5);
+   
+   //timezone
+   date_default_timezone_set(TIME_ZONE);
+
+   //----- Check number -----
+   function check_number($check_str)
+   {
+      if(!is_numeric($check_str))
+      {
+         return SYMBOL_ERROR;
+      }
+      if($check_str < 0)
+      {
+         return SYMBOL_ERROR;
+      }
+      return $check_str;
+   }
+
+   //get data from client
+   $cmd;
+   $UserId;
+
+   //query
+   $link;
+   
+   if (($TrainingId = check_number($_POST["training_id"])) == SYMBOL_ERROR)
+   {
+      sleep(DELAY_SEC);
+      echo DB_ERROR;       
+      return;
+   }
+
+   //link    
+   $link = @mysqli_connect(DB_HOST, ADMIN_ACCOUNT, ADMIN_PASSWORD, CONNECT_DB);    
+   if (!$link)  //connect to server failure    
+   {
+      sleep(DELAY_SEC);
+      echo DB_ERROR;
+      return;
+   }
+
+   $users_id = $_POST["users_id"];// employId & userWId
+   // if ($users_id[0] == "null")
+   // {
+      // $str_query = "delete from examroll where ExamId=$exam_id";
+      // if(!mysqli_query($link, $str_query)){
+         // echo DB_ERROR;
+         // return;
+      // }
+      // echo 0;
+      // return;
+   // }
+
+  if(!($users_id = transfer_all_id_to_user_id($users_id)))
+   {
+      return;
+   }
+   $users_id = array_unique($users_id);
+   
+   $userlist = "";
+   foreach ($users_id as $uid)
+   {
+      $userlist = $userlist . ",$uid,";
+   }
+
+   /////////////////////////////////////
+   //select all current ids
+   //if not exist in upload id,
+   //   set Status = -1
+   //for all upload id
+   //  if exist -> update status to 1
+   //else
+   //  insert it
+   /////////////////////////////////////
+   // $str_query = "select * from examroll where ExamId=$exam_id";
+// 
+   // if($result = mysqli_query($link, $str_query)){
+      // $row_number = mysqli_num_rows($result);
+      // for ($i=0; $i<$row_number; $i++)
+      // {
+         // $row = mysqli_fetch_assoc($result);
+         // if (!in_array($row["UserId"], $users_id))
+         // {
+            // if (($ret = update_examroll_status($row["UserId"], $exam_id, INACTIVE)) != SUCCESS)
+            // {
+               // echo $ret;
+               // return;
+            // }
+         // }
+         // else
+         // {
+            // if (($ret = update_examroll_status($row["UserId"], $exam_id, ACTIVE)) != SUCCESS)
+            // {
+               // echo $ret;
+               // return;
+            // }
+            // if(($key = array_search($row["UserId"], $users_id)) !== false) {
+               // array_splice($users_id, $key, 1);
+            // }
+         // }
+      // }   
+   // }
+
+   // left ids are all new ids, so just insert it
+   // foreach ($users_id as $user_id)
+   // {
+      // $str_query = "INSERT INTO examroll (ExamId,UserId,IsSubmit,Status) VALUES ($exam_id,$user_id,".NOT_SUBMIT.",".ACTIVE.")";
+      // if(!($result = mysqli_query($link, $str_query))){
+         // if($link){
+            // mysqli_close($link);
+         // }
+         // sleep(DELAY_SEC);
+         // echo -__LINE__;
+         // return;
+      // }      
+   // }
+   $str_query = "update Trainings set UserList = '$userlist' where TrainingId = $TrainingId";
+   if(!($result = mysqli_query($link, $str_query))){
+      if($link){
+         mysqli_close($link);
+      }
+      sleep(DELAY_SEC);
+      echo -__LINE__;
+      return;
+   } 
+   
+   echo SUCCESS;
+   return;
+   
+   // function update_examroll_status($user_id, $exam_id, $status)
+   // {
+      // $link = @mysqli_connect(DB_HOST, ADMIN_ACCOUNT, ADMIN_PASSWORD, CONNECT_DB);
+      // if (!$link) 
+      // {   
+         // die(MSG_ERR_CONNECT_TO_DATABASE);
+      // }
+//       
+      // $str_query = "update examroll set Status=$status where ExamId=$exam_id AND UserId=$user_id";
+      // if(($result = mysqli_query($link, $str_query))){
+         // return SUCCESS;
+      // }
+      // else
+      // {
+         // return ERR_UPDATE_DATABASE;
+      // }
+   // }
+   
+   function transfer_all_id_to_user_id($ids)
+   {        
+      $link = @mysqli_connect(DB_HOST, ADMIN_ACCOUNT, ADMIN_PASSWORD, CONNECT_DB);    
+      if (!$link)  //connect to server failure    
+      {
+         sleep(DELAY_SEC);
+         echo DB_ERROR;
+         die("连接DB失败");
+      }
+
+      $users_id = array();
+
+      // if Exxxx, xxxx is digit
+      // else if it is composed by alpha & digit, it is employID, and get it user_id, and push it
+      foreach ($ids as $id)
+      {
+         if (preg_match("/^E[0-9]+$/", $id))
+         {  
+            $str_query = "select * from users where EmployeeId='$id'";
+            if(($result = mysqli_query($link, $str_query))){
+               if ($row = mysqli_fetch_assoc($result))
+               {
+                  array_push($users_id, $row["UserId"]);
+               }
+               else
+               {
+                  echo "不存在工号 $id";
+                  return false;
+               }
+            }
+            else
+            {
+               echo DB_ERROR;
+               die("操作资料库失败");
+            }
+         }
+         else if(preg_match("/^[0-9|a-zA-Z]+$/", $id))
+         { 
+            $str_query = "select * from users where UserWId='$id'";
+            if(($result = mysqli_query($link, $str_query))){
+               if ($row = mysqli_fetch_assoc($result))
+               {
+                  array_push($users_id, $row["UserId"]);
+               }
+               else
+               {
+                  echo "不存在使用者ID $id";
+                  return false;
+               }
+            }
+            else
+            {
+               echo DB_ERROR;
+               die("操作资料库失败");
+            }
+         }
+         else
+         {
+            echo "不存在使用者 $id";
+            return false; 
+         }
+      }  
+      return $users_id;
+   }
+?>
